9f275d108a415d06a784d8f323b74282a3d59192
Volodymyr Babych <domonock13@icloud.com>
2023-12-13 11:55:23 +0200

    Initial Commit

---
+++ Memes/MemesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemesApp.swift
+//  Memes
+//
+//  Created by Volodymyr Babych on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
